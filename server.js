fd2bfae4e6044c937df874abb5f3eaef2b2da3c2
lhk9311 <lhk9311@gmail.com>
2026-05-11 16:15:27 +0900

    1차 완성본

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3001" }
+  cors: { origin: ["http://localhost:3001", "http://localhost:3002"] }
 });
 
 // 👉 라우터 연결
